bd11f5250cfffd30bc8fff0cf6feb09c8ad38c8f
lenoob <78706766+lenoobwastaken@users.noreply.github.com>
2022-08-17 18:53:56 -0700

    Add files via upload

---
+++ LoggerUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VRCEXLOGGER
+{
+    internal static class LoggerUtils
+    {
+        internal static void Log(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine($"[VRCEX] {message}", ConsoleColor.Red);
+          //  Console.Read();
+        }
+    }
+}
